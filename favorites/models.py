f7ebbda8ae0db5c5eee722d008b221844e232ebb
José Sánchez Moreno <jose@o2w.es>
2016-02-29 22:59:38 +0100

    django 1.9

--- favorites/models.py
+++ favorites/models.py
@@ -1,9 +1,9 @@
-#encoding=utf-8
+# encoding=utf-8
 
-from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes import fields
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 if hasattr(settings, 'AUTH_USER_MODEL'):
@@ -13,7 +13,7 @@ else:
 
 
 class FavoriteManager(models.Manager):
- 
+
     def create_favorite(self, user, obj):
         """create Favorite for obj and user.
         """
@@ -52,7 +52,7 @@ class Favorite(models.Model):
     user = models.ForeignKey(User)
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = fields.GenericForeignKey('content_type', 'object_id')
     created_time = models.DateTimeField(auto_now_add=True)
 
     objects = FavoriteManager()
